0aba45876e71281ebf0e3fe8078fae047c5ba5d4
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-07-26 16:18:54 +0200

    Change to overall design and fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
   get 'static_pages/about'
   get 'static_pages/contact'
-  get 'static_pages/index'
-
-  root 'static_pages#index'
 end
