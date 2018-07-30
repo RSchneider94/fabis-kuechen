03f94cee1f369a47c6528a8c49c8ecab511daaa9
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-07-30 20:05:11 +0200

    Add orders controller to routes, created the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :new, :create, :destroy]
   root 'static_pages#landing_page'
   get 'static_pages/about'
   get 'static_pages/contact'
