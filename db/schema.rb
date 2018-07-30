8f0da1d1335768de085b507055868c6c8102c19c
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-07-30 11:37:07 +0200

    Add price field to products table and change the products listing layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_143319) do
+ActiveRecord::Schema.define(version: 2018_07_30_090038) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_26_143319) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
   end
 
 end
