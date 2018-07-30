3b7c8aae2797706ca84a89ede4315bd38904ebbc
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-07-30 19:56:38 +0200

    Create featured product in index and orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
   get 'static_pages/about'
   get 'static_pages/contact'
 end
