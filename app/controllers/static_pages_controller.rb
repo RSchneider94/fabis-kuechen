58362f77d548a86bcdaab2704dbe3e2e88669879
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-08-10 11:29:08 +0200

    Implement back-end contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,16 @@ class StaticPagesController < ApplicationController
   def landing_page
     @products = Product.limit(2)
   end
+
+  def danke
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(
+      from: @email,
+      to: 'rodrigoschneider94@gmail.com',
+      subject: "Neue Nachricht von #{ @name }",
+      body: @message
+    ).deliver_now
+  end
 end
