05372af8c5d56c2f8c2d706631c49e6c1e5443f0
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-08-10 10:48:51 +0200

    Implement validation when using production or development environment to products search

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_query)
-    Product.where("name LIKE ?", "%#{ search_query }%")
+    if Rails.env.production?
+      Product.where("name ilike ?", "%#{ search_query }%")
+    else
+      Product.where("name LIKE ?", "%#{ search_query }%")
+    end
   end
 end
