94d6486175ff37fe34ec98eed4b1c2132017ad92
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-08-13 17:41:46 +0200

    Implement user authentication

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @orders = Order.includes(:product).all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :products
   resources :orders, only: [:index, :show, :new, :create, :destroy]
   root 'static_pages#landing_page'
