959a1b2b9890f1525b636b57a4327ed70b503100
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-09-05 14:52:06 +0200

    Fixed bugs provided by CF mentor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,6 +25,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:user_id, :title, :body, :rating)
+    params.require(:comment).permit(:title, :body, :rating)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,6 @@
 class Comment < ApplicationRecord
   # Validations
   validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
   # Relationships
   belongs_to :user
