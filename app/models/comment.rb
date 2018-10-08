d25fae145d944ea31968d5989b7aa5f6b85613a6
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-10-08 16:00:55 +0200

    New changes for ActiveJob and Job Class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   # Validations
   validates :body, presence: true
   validates :rating, numericality: { only_integer: true }
+  # Jobs
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
   # Relationships
   belongs_to :user
   belongs_to :product
